e2836808a3a7eca12af6cdf34c38a5fe482ed975
RicardoDanta <uruguaydenos@gmail.com>
2022-03-12 21:39:14 -0800

    0

---
+++ _printf.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+* _printf - functions
+* @format: format
+*/
+int _printf(const char *format, ...)
+{
+	va_list lista;
+	int pos = 0;
+	int i = 0;
+	int par = 0;
+	int j;
+
+	if (format)
+	{
+		va_start(lista, format);
+
+		for (j = 0; format[j] != '\0'; j++)
+			
+			if (!pos)
+			{	
+				if (format[j] != '%')
+				{	
+					i += _putchar(format[j]);
+				}
+				else
+				{
+					pos = 1;
+				}
+			}		
+			else
+			{
+				switch (format[j])
+				{
+						case 'c':
+						i += _putchar(va_arg(lista, int));
+						break;
+				}
+			pos = 0;	
+			}
+	}
+
+				return (i);
+}
