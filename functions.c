79677257bfe285e2ccf0ff35590369352e13fe8c
RicardoDanta <uruguaydenos@gmail.com>
2022-03-15 14:05:02 -0700

    ya arregle todo

--- functions.c
+++ functions.c
@@ -19,7 +19,6 @@ int _putchar(char c)
 int _print_s(char *str)
 {
 	int i = 0;
-
 	if (str == NULL)
 		str = "(null)";
 	if (str)
@@ -41,7 +40,6 @@ int _numbers(long int n)
 {
 	int p = 0;
 	long int e = n;
-
 	if (n)
 	{
 		if (e < 0)
@@ -54,32 +52,48 @@ int _numbers(long int n)
 			p += _numbers(e / 10);
 		}
 		p += _putchar((e % 10) + '0');
-
 	}
 	else
 		p += _putchar(n + '0');
 	return (p);
 }
 /**
-* _unsigned - print numbers
+* _numbers - print numbers
 * @n: integer
 * Return: numbers
 */
 int _unsigned(unsigned int n)
-{
-	int p = 0;
-	unsigned int e = n;
+ {
+         int p = 0;
+         unsigned int e = n;
+		if (n)
+       {
+                  if ((e / 10) > 0)
+                  {
+                        p += _numbers(e / 10);
+                  }
+                 p += _putchar((e % 10) + '0');
 
-	if (n)
-	{
-		if ((e / 10) > 0)
-		{
-			p += _numbers(e / 10);
-		}
-		p += _putchar((e % 10) + '0');
-	}
-	else
-		p += _putchar(n + '0');
+          }
+		else
+			p += _putchar(n + '0');
 		return (p);
 }
+/**
+* _binary - print binary numbers
+* @b: number
+* Return: binary
+*/
+int _binary(int b)
+{
+	int u = 1, c = 0, cam;
 
+	while (b != 0)
+	{
+		cam = b % 2;
+		b /= 2;
+		c += cam * u;
+		u *= 10; (u = u * 10);
+	}
+	return (c);
+}
